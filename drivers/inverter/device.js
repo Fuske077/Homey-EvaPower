d35d9279bda8731513e758dd6ce90499eaa93021
Fuske077 <tommyvaessen@gmail.com>
2025-04-13 08:41:24 +0200

    Update device.js

--- drivers/inverter/device.js
+++ drivers/inverter/device.js
@@ -1,10 +1,10 @@
 'use strict';
 const Homey = require('homey');
 const axios = require('axios');
-const utils = require('./utils'); 
+const utils = require('./utils');
 
 class InverterDevice extends Homey.Device {
-	
+
   async onInit() {
     this.log('InverterDevice has been initialized');
     this.sysSn = this.getSetting('sysSn');
@@ -12,16 +12,16 @@ class InverterDevice extends Homey.Device {
 
     this.appId = this.homey.settings.get('appId');
     this.appSecret = this.homey.settings.get('appSecret');
-    this.realtimeDataUrl = `https://openapi.alphaess.com/api/getLastPowerData?sysSn=${this.sysSn}`; 
-	
+    this.realtimeDataUrl = `https://openapi.alphaess.com/api/getLastPowerData?sysSn=${this.sysSn}`;
+
     this.startPolling();
   }
-  
+
   async onSettings({ oldSettings, newSettings, changedKeys }) {
     this.refreshInterval = newSettings.interval < 10 ? 10 : newSettings.interval;
     this.startPolling();
   }
-	
+
   startPolling() {
     if (this.pollingTask) clearInterval(this.pollingTask);
     this.pollingTask = setInterval(() => {
@@ -29,7 +29,7 @@ class InverterDevice extends Homey.Device {
     }, this.refreshInterval * 1000);
   }
 
-  async fetchData() {	  
+  async fetchData() {
     if (!this.appId || !this.appSecret || !this.sysSn) {
       this.log('Missing configuration: appId, appSecret, or sysSn');
       return;
@@ -45,19 +45,38 @@ class InverterDevice extends Homey.Device {
     };
 
     try {
-      // Real-time data ophalen
       const response = await axios.get(this.realtimeDataUrl, { headers });
       const data = response.data;
 
-      await this.setCapabilityValue('measure_power', data.data.pload);
-      await this.setCapabilityValue('measure_battery', data.data.soc);
-      await this.setCapabilityValue('measure_battery_level', data.data.soc);
-      await this.setCapabilityValue('measure_pv_power', data.data.ppv);
-      await this.setCapabilityValue('measure_bat_power', data.data.pbat * -1);
-      await this.setCapabilityValue('measure_grid_power', data.data.pgrid);
+      const soc = data.data.soc;
+      const vermogenAccu = data.data.pbat;
+      const vermogenGrid = data.data.pgrid;
 
-      // Daggegevens ophalen
+      await this.setCapabilityValue('measure_power', data.data.pload);
+      await this.setCapabilityValue('measure_battery', soc);
+      await this.setCapabilityValue('measure_battery_level', soc);
+      await this.setCapabilityValue('measure_pv_power', data.data.ppv);
+      await this.setCapabilityValue('measure_bat_power', vermogenAccu * -1);
+      await this.setCapabilityValue('measure_grid_power', vermogenGrid);
+
       await this.fetchDailySummary(headers);
+
+      // === Berekeningen tijd tot vol / leeg ===
+      const vermogenIn = vermogenGrid > 0 ? vermogenGrid : 0;
+      const vermogenUit = vermogenAccu < 0 ? Math.abs(vermogenAccu) : 0;
+
+      const capaciteit = this.getSetting('accuCapacity') || 2;
+      const minSoc = 10;
+      const maxSoc = 100;
+
+      const energieTotLeeg = ((soc - minSoc) / 100) * capaciteit * 1000;
+      const energieTotVol = ((maxSoc - soc) / 100) * capaciteit * 1000;
+
+      const tijdTotLeeg = vermogenUit > 0 ? Math.round((energieTotLeeg / vermogenUit) * 10) / 10 : null;
+      const tijdTotVol = vermogenIn > 0 ? Math.round((energieTotVol / vermogenIn) * 10) / 10 : null;
+
+      await this.setCapabilityValue('measure_time_to_empty', tijdTotLeeg || 0);
+      await this.setCapabilityValue('measure_time_to_full', tijdTotVol || 0);
 
     } catch (error) {
       this.error('Failed to fetch data:', error);
